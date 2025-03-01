362f060b8734f72ac52edf35aa5af77f0c175463
Siddhesh Shinde <siddheshshinde@Siddheshs-Air.bwrouter>
2025-03-01 18:57:31 +0530

    add base url configuration

--- src/APIService/helper.js
+++ src/APIService/helper.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 import { getCookie } from "../utils/helper";
+import baseURL from './baseURL.json'
+
+let appENV = process.env.REACT_APP_API_ENV || 'development'
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: baseURL[appENV].baseURL,
   headers: {
     authorization: `Bearer ${getCookie("auth")}`
   }
